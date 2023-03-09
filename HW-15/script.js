5fda280f7968e13863cf7c8a1ba34cdb7c65d93c
SonyUA <pankivluck@gmail.com>
2023-03-09 14:18:47 +0200

    'version-2'

--- HW-15/script.js
+++ HW-15/script.js
@@ -1,13 +1,31 @@
-const table = document.createElement("table");
-document.body.prepend(table);
-let innerArr = [];
-// Style
-table.style.border = "1px solid black";
-table.style.margin = "20px auto";
+function addTable(number) {
+    let innerArr = [];
+    let count = number;
+    let remainder = number % 10;
+    const table = document.createElement("table");
+    document.body.prepend(table);
+    let caption = document.createElement("caption");
+    caption.innerHTML = `${number}`;
+    caption.style.fontSize = "30px";
+    table.append(caption);
+    if (typeof number !== "number") {
+        return console.log((table.innerHTML = "Введене не число!!!"));
+    }
+    // Style
+    table.style.border = "1px solid black";
+    table.style.margin = "20px auto";
+    for (let i = 1; i <= number; i++) {
+        innerArr.push(i);
+        if (count === remainder && count === innerArr.length) {
+            addrow();
+        }
+        if (i % 10 === 0 && i !== 0) {
+            addrow();
+        }
+    }
 
-for (let i = 1; i <= 100; i++) {
-    innerArr.push(i);
-    if (i % 10 === 0 && i !== 0) {
+    function addrow() {
+        count -= 10;
         let tr = document.createElement("tr");
         innerArr.forEach((el) => {
             let td = document.createElement("td");
@@ -22,3 +40,5 @@ for (let i = 1; i <= 100; i++) {
         innerArr = [];
     }
 }
+
+addTable(123);
