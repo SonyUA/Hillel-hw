bdc1158f23e5b8574dcc8e7b3146bc1f35ab8e94
SonyUA <pankivluck@gmail.com>
2023-04-26 21:27:31 +0300

    'HW-26-corection'

--- HW-26/script.js
+++ HW-26/script.js
@@ -10,16 +10,19 @@ boxForPost.addEventListener("click", toggle);
 
 async function apiForPost() {
     let key = Number(inputSearch.value);
-    if (key === "" || key <= 0) {
+    if (key === "" || key <= 0 || key > 100) {
         return (boxForPost.innerHTML = `ERROR - ключа з ID  ${key} не існує`);
     }
 
     boxForComments.classList.add("btnToggle");
 
     try {
-        let responsePost = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+        let responsePost = await fetch(`https://jsonplaceholder.typicode.com/posts/${key}`);
         let data = await responsePost.json();
-        data.length >= key ? createPost(data[key - 1]) : (boxForPost.innerHTML = `ERROR - ключа з ID ${key} не існує`);
+        boxForPost.innerHTML = `<h4 class="name"><i class="medium material-icons" style="color:${randomRGB()};">face</i>Name ID -${data.id}</h4>
+                            <p><span class = 'title'>Title:</span><br/>${data.title}</p>
+                            <p class="postText"><span class = 'post'>Post:</span> <br/>${data.body}</p>
+                            <a id="commentBtn" class="waves-effect waves-light btn-small"><i class="material-icons left">comment</i>Коментарі</a>`;
     } catch (error) {
         console.log("ERROR:" + error.message);
     }
@@ -27,25 +30,14 @@ async function apiForPost() {
     try {
         let responseComments = await fetch(`https://jsonplaceholder.typicode.com/posts/${key}/comments`);
         let dataComments = await responseComments.json();
-        createComments(dataComments);
+        boxForComments.innerHTML = "";
+        let comment = "";
+        dataComments.forEach((el) => {
+            comment += `<h6><i class="small material-icons"style="color:${randomRGB()};">face</i>Name ID ${el.id}</h6>
+                    <p class="postText"><span>Comment:</span> <br/>${el.body}</p>`;
+        });
+        boxForComments.innerHTML = comment;
     } catch (error) {
         console.log("ERROR: " + error.message);
     }
-}
-
-function createPost(user) {
-    boxForPost.innerHTML = `<h4 class="name"><i class="medium material-icons" style="color:${randomRGB()};">face</i>Name ID -${user.id}</h4>
-                            <p><span class = 'title'>Title:</span><br/>${user.title}</p>
-                            <p class="postText"><span class = 'post'>Post:</span> <br/>${user.body}</p>
-                            <a id="commentBtn" class="waves-effect waves-light btn-small"><i class="material-icons left">comment</i>Коментарі</a>`;
-}
-
-function createComments(data) {
-    boxForComments.innerHTML = "";
-    let comment = "";
-    data.forEach((el) => {
-        comment += `<h6><i class="small material-icons"style="color:${randomRGB()};">face</i>Name ID ${el.id}</h6>
-                    <p class="postText"><span>Comment:</span> <br/>${el.body}</p>`;
-    });
-    return (boxForComments.innerHTML = comment);
 }
