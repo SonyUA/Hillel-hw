5e66ffcb644e36003b9a983e35f18376677a8e5c
SonyUA <pankivluck@gmail.com>
2023-05-14 12:16:41 +0300

    React-HW1

---
+++ React-HW1/src/App.jsx
@@ -0,0 +1,10 @@
+import { Card } from "./components/card";
+const App = () => {
+    return (
+        <>
+            <Card />
+        </>
+    );
+};
+
+export default App;
---
+++ React-HW1/src/components/CardCount.jsx
@@ -0,0 +1,4 @@
+export const Cardcount = (props) => {
+    // eslint-disable-next-line react/prop-types
+    return <div className='c'>{props.num}</div>;
+};
