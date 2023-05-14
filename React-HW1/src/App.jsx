e2f53fb9151ed3d9acaf6a8ec4f57ec1a7ed4da1
SonyUA <pankivluck@gmail.com>
2023-05-14 12:34:06 +0300

    React-HW1corection

--- React-HW1/src/App.jsx
+++ React-HW1/src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
 import { Card } from "./components/card";
 const App = () => {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <Card />
