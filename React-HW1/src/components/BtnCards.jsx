7560152b8a34897939afab33c1d88ae7e77538c5
SonyUA <pankivluck@gmail.com>
2023-05-22 13:46:55 +0300

    'corection'

---
+++ React-HW1/src/components/BtnCards.jsx
@@ -0,0 +1,11 @@
+const BtnCards = () => {
+    const btnCard = {
+        position: "absolute",
+    };
+    return (
+        <>
+            <button style={btnCard}>CLick Me</button>
+        </>
+    );
+};
+export default BtnCards;
