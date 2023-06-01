6b73883e92a9318cc309f9d458c9fdabb6968cd2
Dima Pankiv <33907147+SonyUA@users.noreply.github.com>
2023-06-01 18:51:11 +0300

    Delete HW-2 directory

--- HW-2/work-4/script.js
+++
@@ -1,2 +0,0 @@
-const hours = prompt("Введіть кількість годин");
-alert(` У ${hours} год є ${hours * 60 * 60} секунд `);
--- HW-2/work-5/script.js
+++
@@ -1,4 +0,0 @@
-const numberOne = Number(prompt("Середнє арифметичне, введіть перше число"));
-const numberTwo = Number(prompt("Середнє арифметичне, введіть друге число"));
-const numberThree = Number(prompt("Середнє арифметичне, введіть третє число"));
-alert(`Середнє арифметичне чисел ${numberOne}, ${numberTwo}, ${numberThree} дорівнює ${(numberOne + numberTwo + numberThree) / 3}`);
